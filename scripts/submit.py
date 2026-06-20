bdc03a310d77b7bc34029682932f59e4b7b588c2
snarfnet <82538502+snarfnet@users.noreply.github.com>
2026-06-20 17:30:54 +0900

    Set APP_ID to 6782303634

--- scripts/submit.py
+++ scripts/submit.py
@@ -2,7 +2,7 @@ import jwt, time, requests, sys
 
 KEY_ID       = 'WDXGY9WX55'
 ISSUER       = '2be0734f-943a-4d61-9dc9-5d9045c46fec'
-APP_ID       = 'TODO_APP_ID'   # ASCで新規アプリ作成後に差し替える
+APP_ID       = '6782303634'
 BUILD_NUMBER = sys.argv[1]
 
 if APP_ID.startswith('TODO'):
